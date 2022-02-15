392b00b3ced205a143fe26bf70ca0ceb32e4aa5a
James Colvin <jamesc127@gmail.com>
2022-02-15 08:50:26 -0600

    Cleaning up repo

---
+++ 01-deploy-dse-cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+helm repo add k8ssandra https://helm.k8ssandra.io/stable
+helm install cass-operator k8ssandra/cass-operator -n cass-operator --create-namespace
+kubectl apply -n cass-operator -f ./datastax_enterprise/deploy-cassandra.yaml
+kubectl apply -n cass-operator -f ./datastax_enterprise/studio-deployment.yaml
+CASSANDRA_PASS=$(kubectl -n cass-operator get secret dse-gke-cluster-superuser -o json | jq -r '.data.password' | base64 --decode)
+# https://github.com/k8ssandra/cass-operator
+# https://github.com/k8ssandra/cass-operator/blob/master/config/samples/example-cassdc-three-nodes-single-rack.yaml
+# https://github.com/k8ssandra/cass-operator/blob/bd779cd4d4101e2216d0e0ed357d8662dc8ad0e8/tests/testdata/solr-dc.yaml
+# https://github.com/k8ssandra/cass-operator/blob/eb7bbb91f9bb4e93547af9821e40dfab6a024390/apis/cassandra/v1beta1/cassandradatacenter_types.go#L608-L618
+# https://github.com/k8ssandra/cass-operator/blob/eb7bbb91f9bb4e93547af9821e40dfab6a024390/apis/cassandra/v1beta1/cassandradatacenter_types.go#L737-L778
+#
--- 01-deploy-stack.sh
+++ 01-deploy-stack.sh
@@ -12,29 +12,29 @@ error() {
   exit 1
 }
 
-add_helm_repos(){
-  helm repo add datastax https://datastax.github.io/charts
-  helm repo add datastax-pulsar https://datastax.github.io/pulsar-helm-chart
-  helm repo add elastic https://helm.elastic.co
-  helm repo update
-}
+#add_helm_repos(){
+#  helm repo add datastax https://datastax.github.io/charts
+#  helm repo add datastax-pulsar https://datastax.github.io/pulsar-helm-chart
+#  helm repo add elastic https://helm.elastic.co
+#  helm repo update
+#}
+#
+#deploy_dse(){
+#  helm install -f ./datastax_enterprise/cass-operator-values.yaml cass-operator datastax/cass-operator
+#  sleep 5
+#  kubectl apply -f ./datastax_enterprise/deploy-cassandra.yaml
+#  kubectl apply -f ./datastax_enterprise/studio-deployment.yaml
+#}
 
-deploy_dse(){
-  helm install -f ./datastax_enterprise/cass-operator-values.yaml cass-operator datastax/cass-operator
-  sleep 5
-  kubectl apply -f ./datastax_enterprise/deploy-cassandra.yaml
-  kubectl apply -f ./datastax_enterprise/studio-deployment.yaml
-}
-
-deploy_elk(){
-  helm install elasticsearch elastic/elasticsearch -f ./elk/elastic-values-gcp.yaml
-  helm install kibana elastic/kibana
-  kubectl apply -f ./elk/kibana-loadbalancer.yaml
-}
-
-deploy_luna_streaming(){
-  helm install pulsar -f ./luna_streaming/pulsar-values-auth-gcp.yaml datastax-pulsar/pulsar
-}
+#deploy_elk(){
+#  helm install elasticsearch elastic/elasticsearch -f ./elk/elastic-values-gcp.yaml
+#  helm install kibana elastic/kibana
+#  kubectl apply -f ./elk/kibana-loadbalancer.yaml
+#}
+#
+#deploy_luna_streaming(){
+#  helm install pulsar -f ./luna_streaming/pulsar-values-auth-gcp.yaml datastax-pulsar/pulsar
+#}
 
 create_cassandra_table_meteorite(){
   kubectl exec cdc-test-dc1-rack1-sts-0 -- cqlsh -u cdc-test-superuser -p $CASSANDRA_PASS -e \
@@ -63,11 +63,11 @@ create_cassandra_table_starbucks(){
   ) WITH cdc=true;"
 }
 
-configure_luna_streaming(){
-  kubectl cp ./luna_streaming/pulsar_configure.sh $(kubectl get pods | grep "pulsar-bastion-*" | awk '{print $1}'):/pulsar/bin/pulsar_configure.sh
-  kubectl exec $(kubectl get pods | grep "pulsar-bastion-*" | awk '{print $1}') -- chmod +x /pulsar/bin/pulsar_configure.sh
-  kubectl exec $(kubectl get pods | grep "pulsar-bastion-*" | awk '{print $1}') -- bash /pulsar/bin/pulsar_configure.sh $CASSANDRA_PASS
-}
+#configure_luna_streaming(){
+#  kubectl cp ./luna_streaming/pulsar_configure.sh $(kubectl get pods | grep "pulsar-bastion-*" | awk '{print $1}'):/pulsar/bin/pulsar_configure.sh
+#  kubectl exec $(kubectl get pods | grep "pulsar-bastion-*" | awk '{print $1}') -- chmod +x /pulsar/bin/pulsar_configure.sh
+#  kubectl exec $(kubectl get pods | grep "pulsar-bastion-*" | awk '{print $1}') -- bash /pulsar/bin/pulsar_configure.sh $CASSANDRA_PASS
+#}
 
 create_cassandra_keyspace(){
   CASSANDRA_PASS=$(kubectl get secret cdc-test-superuser -o json | jq -r '.data.password' | base64 --decode)
@@ -80,17 +80,9 @@ create_cassandra_keyspace(){
 }
 
 start_deploy
-add_helm_repos
-sleep 1
-deploy_dse
-sleep 5
-deploy_elk
-sleep 5
-deploy_luna_streaming
 kubectl wait --for=condition=available --timeout=600s --all deployments
 sleep 60
 create_cassandra_keyspace
 create_cassandra_table_meteorite
 sleep 2
-configure_luna_streaming
 exit 0
